a516e4672803159076a39b16b6eff91bd1b2e32f
CorboPy <alex.corbett48@gmail.com>
2024-02-06 19:44:15 +0000

    Update again + addition of commands.txt

--- client.py
+++ client.py
@@ -12,6 +12,8 @@
 # 3. Data request function
 # 4. Live TCAM stream function
 
+##### CURRENT ERRORS: NEED TO ENCODE UTF-8 FOR JSON STRINGS TOO #####
+
 # https://www.youtube.com/watch?v=79dlpK03t30&list=PLGs0VKk2DiYxdMjCJmcP6jt4Yw6OHK85O&index=48
 
 # process 1 - sending messages
@@ -26,32 +28,51 @@ from funcs import *
 
 data_list=["TCAM","VOLT","TEMP"] # For additional intentifiable data reqs, add them here and then add them to parse_data() in funcs.py!!!!!!
 cmmd_list=["AOCS","CMD2","CMD3"] # For additional intentifiable 4-character cmmd's, add them here and then add them to parse_cmd() in funcs.py!!!!!!
+cmmd_params=[3,2,1]     # NUMBER OF PARAMS FOR COMMAND IN cmmd_list (MUST BE IN SAME ORDER!!!)
 
 server_hostname = 'TABLET-9A2B0OP7'     # Can get around DHCP by knowing server host name? 
 # Finding server IP
 try:
-    server_ip = str(socket.gethostbyname(server_hostname)) 
+    server_ip = socket.gethostbyname(server_hostname)
+    print("Server found at IP ", server_ip)
 except:
     print("Server not found")
 
+server_ip = input("Please input the server IP manually: ")
+
 # Server IP found
-print("Server found at IP ", server_ip)
+
 # Setting up client socket
 GROUNDClient = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 buffersize = 1024
 
 while True:
-    user_input = input("Please input a command: 'DATA' for data request, 'COMMAND' to send a command, 'STREAM' for TCAM stream, 'SHUTDOWN' to shutdown server (implement full Pi p-off later), 'EXIT' to close client")
+    user_input = input("Please input a command: 'DATA' for data request, 'COMMAND' to send a command, 'STREAM' for TCAM stream, 'SHUTDOWN' to shutdown server (implement full Pi p-off later), 'EXIT' to close client: \n")
 
     if (user_input.lower() == "command"):   #check command first, want to be quick
-        print("command...")
-        #need to check/start p2
+        cmmd_req = get_cmmd_req(cmmd_list,cmmd_params)
+        if cmmd_req == False:
+            print("Returning to menu...")
+            continue
+        elif isinstance(cmmd_req, dict) == False:  
+            print("Error: cmmd_req not a dictionary. Returning to menu...")
+            continue
+        else:       # Everything is OK
+            msg = json.dumps(cmmd_req)
+            GROUNDClient.sendto(msg,server_ip)
+            #need to check/start p2
     
     elif (user_input.lower() == "data"):
-        confirm_data = 'n'
-        while (confirm_data.lower() != 'y'):
-            confirm_data = input("Please confirm dataconfig.json is configured correctly: Y/N")
+        confirm_data = ""
+        while (confirm_data.lower() != 'y') or (confirm_data.lower() != 'exit'):   # if no, need to check again
+            confirm_data = input("Please confirm dataconfig.json is configured correctly: Y/N/EXIT")
+        
+        if confirm_data.lower() == "exit":  # exit to menu
+            print("Returning to menu...")
+            continue
+        
         data_req = get_data_req()
+
         if data_req == None:
             print("Error, resetting...")
         else:
@@ -60,8 +81,13 @@ while True:
         #need to check/start p2
 
     elif (user_input.lower() == "stream"):
-        confirm_stream = input("STREAM: To start, type 'True'. To end, type 'End'.")
-        if confirm_stream.lower() == 'true':
+        confirm_stream = input("STREAM: To start, type 'True'. To end, type 'End'. Or, to exit this menu, type 'Exit':")
+        
+        if confirm_stream.lower() == 'exit':
+            print("Returning to menu...")
+            continue
+
+        elif confirm_stream.lower() == 'true':
             msg = json.dumps({"STREAM":True})
             GROUNDClient.sendto(msg,server_ip)
         elif confirm_stream.lower() == 'false':
--- funcs.py
+++ funcs.py
@@ -1,5 +1,8 @@
 # Functions / Processes for client 
 
+
+
+# Listening in parallel process/thread
 def listen(UDPClient, buffersize,server_ip,data_list):    # listen for incoming messages
     while True:
 
@@ -31,20 +34,26 @@ def listen(UDPClient, buffersize,server_ip,data_list):    # listen for incoming 
 
             if keysList[0] == "STREAM":
                 # Stream JSON {"STREAM": [8x8 matrix]}
-                # Send to plot function in process 3
                 print("Is a STREAM data packet")
+                # Send to plot function in process 3
+
             elif len(keysList) == len(data_list):
                 # Data JSON {"TCAM":[8x8],"VOLT":5,"TEMP":25}
-                # Send to 
                 print("Is a DATA packet")
-
+                # Send to data packet process 4
+            else:
+                print("Error: JSON contents not recognised.")
 
 
         else:
             print("Error: message not from RPi (server_ip != resvfrom address)")
 
 
+### Client -> Server ###
+# Data JSON
 def get_data_req():
+    """Checks dataconfig.json is readble and returns config_dict (dict)
+    """
     try:
         with open("dataconfig.json", "r") as jsonfile:
             config_dict = json.load(jsonfile)   # Checking it can be decoded OK before sending to server
@@ -53,8 +62,40 @@ def get_data_req():
         return(None)
 
     if isinstance(config_dict, dict):       
-        print("dataconfig.json read successful.")
+        print("Success: dataconfig.json read successful.")
         return(config_dict)
     else:
         print("Error: dataconfig.json read unsuccessful.")
         return(None)
+    
+# Command JSON
+def get_cmmd_req(cmmd_list,cmmd_params):  # For additional intentifiable commands, add them to cmd_list and then add an extra elif to the parse_cmd() func
+    # Form: {"CMMD":[param1,param2,param3]}
+    success=False
+    while success == False:
+        input_cmmd = input("Please refer to commands.txt and enter a command (case sensitive) in the form: CMMD(param1, param2, param3, etc):")
+
+        cmmd = input_cmmd[0:4]  # Extracting CMMD identifier
+
+        if cmmd.lower() == "exit":  # Exit
+            print("EXIT selected.")
+            success=False
+            return(False)
+
+        try:
+            params = input_cmmd.split("(")[1].split(")")[0].split(",")  #list of ordered params
+            params = [float(i) for i in params]     #converting args to floats
+        except:
+            print("Error: cannot parse inputted command. Incorrect 4-char identifier or params not floatable, or not in form (a,b,c).")
+            continue
+
+        # Next check command and num of params matches cmmd_list and cmmd_params
+        for i in range(len(cmmd_list)):     # For in range of number of commands
+            if cmmd == cmmd_list[i]:         # Check CMMD matches a CMMD from the cmmd_list
+                if len(params) == cmmd_params[i]:       # Check num of params matches num of params for that command from cmmd_params
+                    print("Sucessful command input.")
+                    success=True
+                    return({cmmd:params})
+        
+        # If the code gets here then no match found 
+        print("Error: CMMD not found and/or params does not match required number. Please try again")
